c7a4ec200ad3a629e016c17bd8a59696017d850d
Ray Bellis <ray@isc.org>
2018-06-06 09:05:39 +0100

    WIP compression

--- rrlist.cc
+++ rrlist.cc
@@ -1,3 +1,5 @@
+#include <map>
+
 #include <ldns/ldns.h>
 #include <ldns/host2wire.h>
 
@@ -25,6 +27,47 @@ void RRList::append(const ldns_dnssec_rrsets* rrset)
 		append(rrset->rrs);
 		append(rrset->signatures);
 	}
+}
+
+struct DNameCompare {
+	bool operator()(const ldns_rdf* a, const ldns_rdf* b) {
+		return ldns_dname_compare(a, b) < 0;
+	}
+};
+
+typedef std::map<const ldns_rdf*, uint16_t, DNameCompare> CompressTable;
+typedef std::vector<uint16_t> CompressOffsets;
+
+void dname_to_buffer(ldns_buffer* buf, const ldns_rdf* name, CompressTable& compress, CompressOffsets& offsets)
+{
+	if (ldns_dname_label_count(name) == 0) {
+		ldns_buffer_write_u8(buf, 0);
+		return;
+	}
+
+	auto iter = compress.find(name);
+	if (iter != compress.end()) {
+		auto pos = iter->second;
+		offsets.push_back(pos);
+		ldns_buffer_write_u16(buf, pos | 0xc000);
+		return;
+	}
+
+	uint16_t pos = ldns_buffer_position(buf);
+	if (pos < 16384) {
+		auto clone = ldns_rdf_clone(name);
+		compress[clone] = pos;
+	}
+
+	auto label = ldns_dname_label(name, 0);
+	auto rest = ldns_dname_left_chop(name);
+	auto size = ldns_rdf_size(label) - 1;
+	auto data = ldns_rdf_data(label);
+
+	ldns_buffer_write(buf, data, size);
+	ldns_rdf_deep_free(label);
+
+	dname_to_buffer(buf, rest, compress, offsets);
 }
 
 size_t RRList::to_buffer_wire(ldns_buffer* buf, int section, bool sigs) const
