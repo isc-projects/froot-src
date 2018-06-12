6cc30f7a1e42c6809858a337b7d1ecdf274a6dd4
Ray Bellis <ray@isc.org>
2018-06-12 12:55:32 +0100

    record TC distribution

--- lightbench.cc
+++ lightbench.cc
@@ -9,7 +9,9 @@
 
 void worker(const Zone& zone, const QueryFile& queries)
 {
-	std::map<int, uint64_t> rcodes;
+	std::map<uint16_t, uint64_t> rcode_count;
+	std::map<bool, uint64_t> tc_count;
+
 	{
 		std::vector<iovec> iov;
 		iov.reserve(5);
@@ -25,16 +27,26 @@ void worker(const Zone& zone, const QueryFile& queries)
 				Context ctx(zone, in);
 				(void) ctx.execute(iov);
 				if (iov.size() >= 1) {
+
 					auto p = reinterpret_cast<uint8_t*>(iov[0].iov_base);
+
 					auto rcode = p[3] & 0x0f;
-					++rcodes[rcode];
+					auto tc = !!(p[2] & 0x02);
+
+					++rcode_count[rcode];
+					++tc_count[tc];
+
 				}
 			}
 		}
 	}
 
-	for (const auto it: rcodes) {
-		std::cerr << it.first << " : " << it.second << std::endl;
+	for (const auto it: rcode_count) {
+		std::cerr << "rcode " << it.first << " : " << it.second << std::endl;
+	}
+
+	for (const auto it: tc_count) {
+		std::cerr << "tc " << it.first << " : " << it.second << std::endl;
 	}
 }
 
